6f7117630df4abc93010b616d33f131531f7c9ea
Jaden <jaden.bhakti@ontariotechu.net>
2021-10-07 15:54:42 -0400

    final changes to modify color

--- Engines_Assignment1/Assets/Plugins/MidtermPlugin.cs
+++ Engines_Assignment1/Assets/Plugins/MidtermPlugin.cs
@@ -9,14 +9,27 @@ public class MidtermPlugin : MonoBehaviour
     [DllImport("MidtermPlugin")]
     private static extern int PrintANumber();
 
+    //actually for modifying color, not win/loss
     [DllImport("MidtermPlugin")]
     private static extern float ModifyWinLoss(float f1, float f2);
+
+    [SerializeField] private Material mat;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            mat.color = Color.black;
+        }
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(PrintANumber());
+
+        //modify color, not win loss
         Debug.Log(ModifyWinLoss(3.0f, 5.0f));
     }
 }
